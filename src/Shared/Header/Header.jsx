17787b2e5e09e805fb34f8d98593b5bc3378519a
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-06-08 12:40:25 +0600

    footer section added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
                 to='/'
                 className={({ isActive }) =>
                     isActive
-                        ? "text-blue-600 border-b-4 border-blue-600"
+                        ? "text-accent border-b-4 border-blue-600"
                         : "hover:border-b-4 hover:border-accent"
                 }
             >
@@ -40,7 +40,7 @@ const Header = () => {
                 to='/instructors'
                 className={({ isActive }) =>
                     isActive
-                        ? "text-blue-600 border-b-4 border-blue-600"
+                        ? "text-accent border-b-4 border-blue-600"
                         : "hover:border-b-4 hover:border-accent"
                 }
             >
@@ -52,7 +52,7 @@ const Header = () => {
                 to='/classes'
                 className={({ isActive }) =>
                     isActive
-                        ? "text-blue-600 border-b-4 border-blue-600"
+                        ? "text-accent border-b-4 border-blue-600"
                         : "hover:border-b-4 hover:border-accent"
                 }
             >
@@ -64,7 +64,7 @@ const Header = () => {
                 to='/dashboard'
                 className={({ isActive }) =>
                     isActive
-                        ? "text-blue-600 border-b-4 border-blue-600"
+                        ? "text-accent border-b-4 border-blue-600"
                         : "hover:border-b-4 hover:border-accent"
                 }
             >
@@ -85,7 +85,7 @@ const Header = () => {
                     to='/login'
                     className={({ isActive }) =>
                         isActive
-                            ? "text-blue-600 border-b-4 border-blue-600"
+                            ? "text-accent border-b-4 border-blue-600"
                             : "hover:border-b-4 hover:border-accent"
                     }
                 >
@@ -95,20 +95,20 @@ const Header = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-black bg-opacity-70">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm text-white dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {listItem}
                     </ul>
                 </div>
                 <Link to='/' className='font-medium text-xl'><span className='text-orange-600'>Sports</span><span className='text-accent'>Mania</span></Link>
             </div>
             <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul className="menu menu-horizontal text-white px-1">
                     {listItem}
                 </ul>
             </div>
