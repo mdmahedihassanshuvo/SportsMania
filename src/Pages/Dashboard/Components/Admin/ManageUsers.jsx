8a4b9648e392aaa2711ed5115308ffad1dc33bee
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-06-09 09:40:01 +0600

    use useAxios hook for intarcept user authorization

--- src/Pages/Dashboard/Components/Admin/ManageUsers.jsx
+++ src/Pages/Dashboard/Components/Admin/ManageUsers.jsx
@@ -1,15 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaUser, FaUserAlt, FaUserGraduate, FaUserShield } from 'react-icons/fa';
 import Swal from 'sweetalert2';
+import { AuthContext } from '../../../../Provider/AuthProvider';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
 
 const ManageUsers = () => {
 
+    const { user, loading } = useContext(AuthContext)
+    // const token = localStorage.getItem('token');
+    const [axiosSecure] = useAxiosSecure()
     const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
+        enabled: !loading,
         queryFn: async () => {
-            const res = await axios('http://localhost:5000/users')
+            const res = await axiosSecure(`/users?email=${user?.email}`)
             return res.data
         }
     })
@@ -55,8 +61,8 @@ const ManageUsers = () => {
                                 <th>{index + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>{user?.roll === 'admin' ? 'Admin' : <button onClick={() => handleAdmin(user)} className='btn bg-blue-600 text-white btn-sm'><FaUserShield/></button>}</td>
-                                <td>{user?.roll === 'instructor' ? 'Instructor' : <button onClick={() => handleInstructor(user)} className='btn bg-orange-600 text-white btn-sm'><FaUserGraduate/></button>}</td>
+                                <td>{user?.roll === 'admin' ? 'Admin' : <button onClick={() => handleAdmin(user)} className='btn bg-blue-600 text-white btn-sm'><FaUserShield /></button>}</td>
+                                <td>{user?.roll === 'instructor' ? 'Instructor' : <button onClick={() => handleInstructor(user)} className='btn bg-orange-600 text-white btn-sm'><FaUserGraduate /></button>}</td>
 
                             </tr>)
                         }
